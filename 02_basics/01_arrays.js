590da4de654464edea2abaf8a17129c25dcad9d8
Aashirbad08 <129428017+Aashirbad08@users.noreply.github.com>
2024-06-29 20:56:37 +0530

    01_array updated

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -90,5 +90,49 @@ console.log("C",myarr);// C [ 1, 6, 7 ]
 And in the splice method the range is included....BUT there is a main difference between these two methods-
 That the splice methods manipulates the original array that it removes the elements where as the slice
 Methods dont.*/
- 
 
+/*
+As we discussed earlier that there are so many methods of arrays in javascript:-
+at: ƒ at()
+concat: ƒ concat()
+constructor: ƒ Array()
+copyWithin: ƒ copyWithin()
+entries: ƒ entries()
+every: ƒ every()
+fill: ƒ fill()
+filter: ƒ filter()
+find: ƒ find()
+findIndex: ƒ findIndex()
+findLast: ƒ findLast()
+findLastIndex: ƒ findLastIndex()
+flat: ƒ flat()
+flatMap: ƒ flatMap()
+forEach: ƒ forEach()
+includes: ƒ includes()
+indexOf: ƒ indexOf()
+join: ƒ join()
+keys: ƒ keys()
+lastIndexOf: ƒ lastIndexOf()
+length: 0
+map: ƒ map()
+pop: ƒ pop()
+push: ƒ push()
+reduce: ƒ reduce()
+reduceRight: ƒ reduceRight()
+reverse: ƒ reverse()
+shift: ƒ shift()
+slice: ƒ slice()
+some: ƒ some()
+sort: ƒ sort()
+splice: ƒ splice()
+toLocaleString: ƒ toLocaleString()
+toReversed: ƒ toReversed()
+toSorted: ƒ toSorted()
+toSpliced: ƒ toSpliced()
+toString: ƒ toString()
+unshift: ƒ unshift()
+values: ƒ values()
+with: ƒ with()
+Symbol(Symbol.iterator): ƒ values()
+*/
+//These are the array methods which will be practised within the upcoming future practises.
